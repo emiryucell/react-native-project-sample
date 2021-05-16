f12e0aa993f6449c6706fa432c448b9101bcae13
Emir Talha Yücel <e.talhayucel@gmail.com>
2021-05-16 22:15:43 +0300

    Product listing page has been created, designed and products pulled from Northwind Vercel Product API.

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React,{useState,useEffect}  from 'react';
+import { Button, StyleSheet, Text, View,ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { ListItem ,Icon } from "react-native-elements";
 
 const Tab = createBottomTabNavigator(
 
@@ -29,9 +30,66 @@ function CategoryScreen({ navigation }) {
   );
 }
 function ProductScreen({ navigation }) {
+
+
+const [products,setProducts]=useState([]);
+
+useEffect(() => {
+
+  fillData();
+ 
+}, [])
+
+
+const fillData=()=>{
+  //HTTP GET
+  fetch('https://northwind.vercel.app/api/products/')
+ .then((res)=>res.json())
+ .then((data)=> {
+
+  
+ setProducts(data);
+         
+ })
+}
+
+
+const deleteProduct=(id)=>{
+  //HTTP DELETE
+          let requestoptions={
+      method:'DELETE',
+      body:JSON.stringify({id:id})
+      }
+      fetch('https://northwind.vercel.app/api/products/'+id,requestoptions)
+      .then((res)=>res.json())
+      .then((data)=>{
+          
+          fillData();
+      })
+      }
+
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>PRODUCTS!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFFFFF'}}>
+
+      <ScrollView >
+          {
+                    products.map((item)=> (
+
+                        <ListItem style={{flex:6}}>
+                            <ListItem.Content style={styles.products} >
+                                <ListItem.Title style={{color:"white"}}>{item.name}</ListItem.Title>
+                                <ListItem.Subtitle style={{color:"white"}}>Quantity Per Unit: {item.quantityPerUnit}</ListItem.Subtitle>
+                                <ListItem.Subtitle style={{color:"white"}}>Unit Price: {item.unitPrice}$</ListItem.Subtitle> 
+                             </ListItem.Content>
+                             <ListItem.Content style={{flex:1}} >
+                             <Icon name="delete"  onPress={()=>deleteProduct(item.id)}/>
+                             </ListItem.Content>
+                        </ListItem>
+            
+                    ))
+                }
+                </ScrollView>
     </View>
   );
 }
@@ -83,5 +141,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  products:{
+flex:5,
+padding:5,
+borderRadius:10,
+    backgroundColor:"blue"
   },
 });
