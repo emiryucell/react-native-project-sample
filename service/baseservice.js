c279812c476e1ad03d4360fa3d3ec431e54511cb
JBerat <berat.99@hotmail.com>
2021-05-22 13:43:09 +0300

    baseservice.js and config.js added

---
+++ service/baseservice.js
@@ -0,0 +1,43 @@
+import config from "../environment/config"
+
+const manager = {
+    
+    get: async (url) => {
+
+        let responsedata;
+
+        await fetch(config.BASE_URL + url)
+        .then((res) => res.json())
+        .then((data) => {
+            responsedata = data;
+        });
+
+        return responsedata;
+
+    },
+
+    post: async(url, data) => {
+
+        let resultdata;
+        let requestOptions = {
+            method:"POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        }
+
+        await fetch(config.BASE_URL + url, requestOptions)
+        .then((res) => res.json())
+        .then((data) => {
+            resultdata = data;
+        });
+
+        return resultdata;
+
+    }
+
+}
+
+export default manager;
