748953571e470d7c37006f4240d773913609c5a1
JBerat <berat.99@hotmail.com>
2021-05-23 19:07:26 +0300

    config.js fixed

--- environment/config.js
+++ environment/config.js
@@ -1,7 +1,22 @@
+import { View, Dimensions, TouchableOpacity, TextInput, StyleSheet, Text, PixelRatio, Image, ActivityIndicator } from 'react-native';
+import { Platform } from 'react-native';
+
 const config = {
     
     BASE_URL:"https://northwind.vercel.app/",
 
 }
 
+const scale = PhoneWidth / 320;
+export const responsiveSize = (size) => {
+  const newSize = size * scale
+  if (Platform.OS === 'ios') {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize))
+  } else {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
+  }
+}
+
+export const PhoneHeight = Dimensions.get("window").height;
+export const PhoneWidth  = Dimensions.get("window").width;
 export default config;
