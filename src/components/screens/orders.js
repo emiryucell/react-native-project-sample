264f886b0370c6dd3367212897371ef3d115e0ad
Emir Talha Yücel <e.talhayucel@gmail.com>
2021-05-23 23:33:42 +0300

    getting orders data

--- src/components/screens/orders.js
+++ src/components/screens/orders.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { ScrollView, View } from 'react-native';
+import { ListItem } from "react-native-elements";
+import manager from '../../../service/baseservice';
 
 function Order({ navigation }) {
+
+  const [orders,setOrders]=useState([]);
+
+  useEffect(() => {
+
+    fillData();
+   
+  }, [])
+
+  const fillData=()=>{
+    
+    manager.get("api/orders")
+    .then((data)=>{
+      setOrders(data);
+          
+   })
+  }
+
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Order!</Text>
+      <View>
+       
       </View>
     );
   }
