3e0397cacd7bf4b49f249cd5b55a22b12d67a76a
JBerat <berat.99@hotmail.com>
2021-05-23 19:11:03 +0300

    addCategories pages added and fixed for navigator

---
+++ src/components/screens/addCategories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function addCategories() {
+    return (
+        <View>
+            <Text>deneme</Text>
+        </View>
+    )
+}
+
