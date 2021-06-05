dbdcce157370689d29889c183a7b922ed428d25e
JBerat <berat.99@hotmail.com>
2021-06-05 14:37:11 +0300

    header made dynamic

---
+++ src/components/header/header.js
@@ -0,0 +1,10 @@
+
+
+const navigationOptions = {
+    headerTitleStyle: { alignSelf: 'center' },
+    headerStyle: { backgroundColor: "#9AC4F8" },
+    headerTintColor: "white",
+    headerBackTitle: "Back",
+  }
+
+  export default navigationOptions;
