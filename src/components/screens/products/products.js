e43c4a59848ad207ce63b024f0f55a28af1c0025
Emir Talha Yücel <e.talhayucel@gmail.com>
2021-06-05 15:07:45 +0300

    having a global design structure and products styles move to global.js

--- src/components/screens/products/products.js
+++ src/components/screens/products/products.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { ListItem } from "react-native-elements";
 import manager from '../../../../service/baseservice';
+import {globalStyles} from "../../styles/global";
 
 
 function Products({ navigation }) {
@@ -9,16 +10,11 @@ function Products({ navigation }) {
 
   const [products, setProducts] = useState([]);
 
-
-
-
   useEffect(() => {
 
     fillData();
 
   }, [])
-
-
 
   const fillData = () => {
     manager.get("api/products/")
@@ -26,47 +22,42 @@ function Products({ navigation }) {
         setProducts(data);
 
       })
-
-
   }
   const deleted = (id) => {
     manager.delete("api/products/",id)
       .then((data)=> {
         fillData();
       })
-
   }
-  
-
-
   return (
-  <View style={styles.container}>
+  <View style={globalStyles.container}>
 
   <ScrollView >
         {
           products.map((item) => (
 
-    <ListItem containerStyle={{ backgroundColor: "#6fa3f7",flex:1,flexDirection:"row"}}>
-      <View style={{flex:1,flexDirection:"row"}}>
+    <ListItem containerStyle={globalStyles.container}>
+      <View style={globalStyles.productStructure}>
         <View style={{flex:1}}>
-              <ListItem.Content style={styles.products} >
-                <ListItem.Title style={{ color: "white" }}>{item.name}</ListItem.Title>
-                <ListItem.Subtitle style={{ color: "white" }}>Quantity Per Unit: {item.quantityPerUnit}</ListItem.Subtitle>
-                <ListItem.Subtitle style={{ color: "white" }}>Unit Price: {item.unitPrice}$</ListItem.Subtitle>
+              <ListItem.Content style={globalStyles.products} >
+                <ListItem.Title style={globalStyles.text}>{item.name}</ListItem.Title>
+                <ListItem.Subtitle style={globalStyles.text}>Quantity Per Unit: {item.quantityPerUnit}</ListItem.Subtitle>
+                <ListItem.Subtitle style={globalStyles.text}>Unit Price: {item.unitPrice}$</ListItem.Subtitle>
               </ListItem.Content>
         </View>
         <View>
               <ListItem.Content>
-                <ListItem.Content  >
-                  <TouchableOpacity onPress={() => navigation.navigate("Product Detail", { id: item.id })} style={styles.detailButton}>
-                    <Text style={{ color: "#367ff5",fontWeight: "bold" }}>Detail</Text>
+                <ListItem.Content>
+                  <TouchableOpacity onPress={() => navigation.navigate("Product Detail", { id: item.id })} 
+                  style={globalStyles.detailButton}>
+                    <Text style={globalStyles.productsButtonText}>Detail</Text>
                   </TouchableOpacity>
 
                 </ListItem.Content>
-                <ListItem.Content style={{ flex: 1 }} >
+                <ListItem.Content>
 
-                  <TouchableOpacity onPress={() => deleted(item.id)} style={styles.deleteButton}>
-                    <Text style={{ color: "#367ff5" ,fontWeight: "bold"}}>Delete</Text>
+                  <TouchableOpacity onPress={() => deleted(item.id)} style={globalStyles.deleteButton}>
+                    <Text style={globalStyles.productsButtonText}>Delete</Text>
                   </TouchableOpacity>
                 </ListItem.Content>
               </ListItem.Content>
@@ -81,46 +72,6 @@ function Products({ navigation }) {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#6fa3f7',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  products: {
-  
-    padding: 5,
-    borderTopLeftRadius: 10,
-    borderBottomLeftRadius: 10,
-    backgroundColor: "#367ff5"
-  },
-  detailButton: {
-    flex: 1,
-    padding: 5,
-    backgroundColor: "#bae0ff",
-  
-    paddingRight: 9.0,
-  
-    borderTopRightRadius: 10,
-    borderColor:"#367ff5",
-    borderWidth:2,
-
-
-  },
-  deleteButton: {
-
-    flex: 1,
-    
-    padding: 5,
-   
-    backgroundColor: "#bae0ff",
-    borderBottomRightRadius: 10,
-    borderColor:"#367ff5",
-    borderWidth:2,
-
-  }
-});
 
 
 export default Products;
---
+++ src/components/styles/global.js
@@ -0,0 +1,45 @@
+import {StyleSheet} from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#6fa3f7',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    products: {
+      padding: 5,
+      borderTopLeftRadius: 10,
+      borderBottomLeftRadius: 10,
+      backgroundColor: "#367ff5"
+    },
+    productStructure:{
+        flex:1,
+        flexDirection:"row"
+    },
+    productsButtonText:{
+        color:"#367ff5",
+        fontWeight:"bold"
+    },
+    text:{
+        color: "white" 
+    },
+    detailButton: {
+      flex: 1,
+      padding: 5,
+      backgroundColor: "#bae0ff",
+      paddingRight: 9.0,
+      borderTopRightRadius: 10,
+      borderColor:"#367ff5",
+      borderWidth:2,
+    },
+    deleteButton: {
+      flex: 1,
+      padding: 5,
+      backgroundColor: "#bae0ff",
+      borderBottomRightRadius: 10,
+      borderColor:"#367ff5",
+      borderWidth:2,
+    }
+  });
+  
